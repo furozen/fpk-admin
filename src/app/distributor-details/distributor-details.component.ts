7bb0985022899dd6bc846e5a1014d465e56e7373
Bogdan <dastan_krimea@mail.ru>
2019-09-29 13:08:57 +0300

    adminka

---
+++ src/app/distributor-details/distributor-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-details',
+  templateUrl: './distributor-details.component.html',
+  styleUrls: ['./distributor-details.component.css']
+})
+export class DistributorDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
